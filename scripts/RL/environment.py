f1f2ef6e01933b7527ee03ab64645fe43aee5fa8
AndMit <andreas6899@gmail.com>
2022-05-05 15:27:05 +0300

    split to classes

--- scripts/RL/environment.py
+++ scripts/RL/environment.py
@@ -13,118 +13,8 @@ from tensorflow.keras import layers
 from mavros_msgs.msg import PositionTarget
 import pylab
 from stalker.msg import PREDdata
-from BoxToLineClass import line_detector
-
-#-------------------------------- NOISE CLASS --------------------------------#
-
-
-class OUActionNoise:
-    def __init__(self, mean, std_deviation, theta=0.15, dt=1e-1, x_initial=None):
-        self.theta = theta
-        self.mean = mean
-        self.std_dev = std_deviation
-        self.dt = dt
-        self.x_initial = x_initial
-        self.reset()
-
-    def __call__(self):
-        # Formula taken from https://www.wikipedia.org/wiki/Ornstein-Uhlenbeck_process.
-        x = (
-            self.x_prev
-            + self.theta * (self.mean - self.x_prev) * self.dt
-            + self.std_dev * np.sqrt(self.dt) * np.random.normal(size=self.mean.shape)
-        )
-        # Store x into x_prev
-        # Makes next noise dependent on current one
-        self.x_prev = x
-        return x
-
-    def reset(self):
-        if self.x_initial is not None:
-            self.x_prev = self.x_initial
-        else:
-            self.x_prev = np.zeros_like(self.mean)
-
-#-------------------------------- CLASS BUFFER --------------------------------#
-
-class Buffer:
-    def __init__(self, buffer_capacity = 100000, batch_size = 64):
-
-        #Number of experiences to store at max
-        self.buffer_capacity = buffer_capacity
-        
-        #Number of tuples to train on
-        self.batch_size = batch_size
-
-        #Number of times record() was called
-        self.buffer_counter = 0
-
-        #We use different np.arrays for each tuple element
-        self.state_buffer = np.zeros((self.buffer_capacity, num_states))
-        self.action_buffer = np.zeros((self.buffer_capacity, num_actions))
-        self.reward_buffer = np.zeros((self.buffer_capacity, 1))
-        self.next_state_buffer = np.zeros((self.buffer_capacity, num_states))
-
-    #Takes (s,a,r,s') observation tuple as input
-    def record(self, obs_tuple):  
-
-        #Set index to zeros if buffer_capacity is exceeded and replace old records
-        index = self.buffer_counter % self.buffer_capacity
-
-        self.state_buffer[index] = obs_tuple[0]
-        self.action_buffer[index] = obs_tuple[1]
-        self.reward_buffer[index] = obs_tuple[2]
-        self.next_state_buffer[index] = obs_tuple[3]
-
-        self.buffer_counter += 1  
-
-    def learn(self):
-
-        # Get sampling range
-        record_range = min(self.buffer_counter, self.buffer_capacity)
-
-        # Randomly sample indices
-        batch_indices = np.random.choice(record_range, self.batch_size)
-
-        # Convert to tensors
-        state_batch = tf.convert_to_tensor(self.state_buffer[batch_indices])
-        action_batch = tf.convert_to_tensor(self.action_buffer[batch_indices])
-        reward_batch = tf.convert_to_tensor(self.reward_buffer[batch_indices])
-        reward_batch = tf.cast(reward_batch, dtype=tf.float32)
-        next_state_batch = tf.convert_to_tensor(self.next_state_buffer[batch_indices])
-
-        self.update(state_batch, action_batch, reward_batch, next_state_batch)
-
-    # Eager execution is turned on by default in TensorFlow 2. Decorating with tf.function allows
-    # TensorFlow to build a static graph out of the logic and computations in our function.
-    # This provides a large speed up for blocks of code that contain many small TensorFlow operations such as this one.
-    @tf.function
-    def update(self, state_batch, action_batch, reward_batch, next_state_batch):
-        # Training and updating Actor & Critic networks.
-
-        with tf.GradientTape() as tape:
-
-            target_actions = target_actor(next_state_batch, training=True)
-            #Compute the real expected return
-            y = reward_batch + gamma * target_critic([next_state_batch, target_actions], training=True) 
-            #Define the output of the critic according to the current batch
-            critic_value = critic_model([state_batch, action_batch], training=True)
-            #Define the Loss Function (real expected return - output of critic)**2
-            critic_loss = tf.math.reduce_mean(tf.math.square(y - critic_value))
-
-        #Do gradient ascent to the critic model according to the loss
-        critic_grad = tape.gradient(critic_loss, critic_model.trainable_variables)
-        critic_optimizer.apply_gradients(zip(critic_grad, critic_model.trainable_variables))
-
-        with tf.GradientTape() as tape:
-            actions = actor_model(state_batch, training=True)
-            critic_value = critic_model([state_batch, actions], training=True)
-            # Used `-value` as we want to maximize the value given (we want gradient ascent, but by default gradient descent is used)
-            # by the critic for our actions
-            actor_loss = -tf.math.reduce_mean(critic_value)
-
-        actor_grad = tape.gradient(actor_loss, actor_model.trainable_variables)
-        actor_optimizer.apply_gradients(zip(actor_grad, actor_model.trainable_variables))  
+from boxtoline import Line_detector
+from util import *
 
 #-------------------------------- CLASS ENVIRONMENT --------------------------------#
 
@@ -136,7 +26,7 @@ class Environment:
         self.pub_pos = rospy.Publisher("/mavros/setpoint_raw/local",PositionTarget,queue_size=10000)
         self.pub_action = rospy.Publisher("/mavros/setpoint_raw/attitude", AttitudeTarget, queue_size=10000)
         
-        # Initialize yaw to zero
+        # Initialize yaw
         self.initial_pose()
 
         # Reset to initial positions
@@ -192,59 +82,11 @@ class Environment:
         action_mavros = AttitudeTarget()
         action_mavros.type_mask = 7
         action_mavros.thrust = 0.5 # Altitude hold
-        action_mavros.orientation = self.rpy2quat(0.0,0.0,90.0) # 90 yaw
-        self.pub_action.publish(action_mavros)
-
-
-    # Convert roll, pitch, yaw (in degrees) to quaternion
-    def rpy2quat(self,roll,pitch,yaw):
-        
-        q = Quaternion()
-        r = np.deg2rad(roll)
-        p = np.deg2rad(pitch)
-        y = np.deg2rad(yaw)
-
-        cy = math.cos(y * 0.5)
-        sy = math.sin(y * 0.5)
-        cp = math.cos(p * 0.5)
-        sp = math.sin(p * 0.5)
-        cr = math.cos(r * 0.5)
-        sr = math.sin(r * 0.5)
-
-        q.w = cr * cp * cy + sr * sp * sy
-        q.x = sr * cp * cy - cr * sp * sy
-        q.y = cr * sp * cy + sr * cp * sy
-        q.z = cr * cp * sy - sr * sp * cy
-
-        return q   
-
-    # Convert quaternion to roll,pitch,yaw (degrees)
-    def quat2rpy(self,quat):
-
-        sinr_cosp = 2.0*(quat.w*quat.x + quat.y*quat.z)
-        cosr_cosp = 1 - 2*(quat.x*quat.x + quat.y*quat.y)
-        roll = math.atan2(sinr_cosp , cosr_cosp)    
-
-        sinp = 2*(quat.w*quat.y - quat.z*quat.x)
-        if abs(sinp)>=1:
-            pitch = math.pi/2.0 * sinp/abs(sinp)
-        else:
-            pitch = math.asin(sinp)
-
-        siny_cosp = 2*(quat.w*quat.z + quat.x*quat.y)
-        cosy_cosp = 1 - 2*(quat.y*quat.y + quat.z*quat.z)
-        yaw = math.atan2(siny_cosp,cosy_cosp)
-
-        roll = np.rad2deg(roll)
-        pitch = np.rad2deg(pitch)
-        yaw = np.rad2deg(yaw)  
-
-        return roll, pitch, yaw      
+        action_mavros.orientation = rpy2quat(0.0,0.0,90.0) # 90 yaw
+        self.pub_action.publish(action_mavros)     
 
     def go_to_start(self):
-        #go to the last point when you had a good detection
-        #that point is stored in x/y/z initial
-        # print('going to start')
+        #go to the last point when had a good detection
         position_reset = PositionTarget()
         position_reset.type_mask = 2496
         position_reset.coordinate_frame = 1
@@ -308,7 +150,7 @@ class Environment:
         self.y_angular = self.position.twist.twist.angular.y
 
         quat = self.position.pose.pose.orientation
-        self.roll, self.pitch, self.yaw = self.quat2rpy(quat)
+        self.roll, self.pitch, self.yaw = quat2rpy(quat)
         self.new_pose = True
 
     def DetectCallback(self, msg):
@@ -352,7 +194,7 @@ class Environment:
                     action_mavros = AttitudeTarget()
                     action_mavros.type_mask = 7
                     action_mavros.thrust = 0.5 # Altitude hold
-                    action_mavros.orientation = self.rpy2quat(0.0,0.0,self.yaw_initial) 
+                    action_mavros.orientation = rpy2quat(0.0,0.0,self.yaw_initial) 
                     self.pub_action.publish(action_mavros)
                     if abs(self.yaw - self.yaw_initial)<10 :
                         self.reset()                 
@@ -461,137 +303,8 @@ class Environment:
                 action_mavros = AttitudeTarget()
                 action_mavros.type_mask = 7
                 action_mavros.thrust = 0.5
-                action_mavros.orientation = self.rpy2quat(roll_des,pitch_des,yaw_des)
+                action_mavros.orientation = rpy2quat(roll_des,pitch_des,yaw_des)
                 self.pub_action.publish(action_mavros)
 
                 self.previous_state = self.current_state
                 self.timestep += 1        
-
-#-------------------------------- MAIN --------------------------------#
-
-
-# This update target parameters slowly
-# Based on rate `tau`, which is much less than one.
-@tf.function
-def update_target(target_weights, weights, tau):
-    for (a, b) in zip(target_weights, weights):
-        a.assign(b * tau + a * (1 - tau)) 
-
-
-def get_actor():
-
-    # Initialize weights between -3e-3 and 3-e3
-    last_init = tf.random_uniform_initializer(minval=-0.003, maxval=0.003)
-
-    inputs = layers.Input(shape=(num_states,))
-    h1 = layers.Dense(128, activation="tanh")(inputs)
-    h2 = layers.Dense(128, activation="tanh")(h1)    
-    outputs = layers.Dense(num_actions, activation="tanh", kernel_initializer=last_init)(h2)
-
-    # Output of tanh is [-1,1] so multiply with the upper control action
-    outputs = outputs * [angle_max, angle_max, yaw_max]
-        
-    model = tf.keras.Model(inputs, outputs)
-
-    return model  
-
-def get_critic():
-
-    # The critic NN has 2 inputs: the states and the actions. Use 2 seperate NN and then concatenate them
-    # State as input
-    state_input = layers.Input(shape=(num_states))
-    h1_state = layers.Dense(16, activation="relu")(state_input)
-    state_out = layers.Dense(32, activation="relu")(h1_state)
-
-    # Action as input
-    action_input = layers.Input(shape=(num_actions))
-    action_out = layers.Dense(32, activation="relu")(action_input)
-
-    # Both are passed through seperate layer before concatenating
-    concat = layers.Concatenate()([state_out, action_out])
-
-    out = layers.Dense(128, activation="relu")(concat)
-    out = layers.Dense(128, activation="relu")(out)
-    outputs = layers.Dense(1)(out)
-
-    # Outputs single value for give state-action
-    model = tf.keras.Model([state_input, action_input], outputs)
-
-    return model 
-
-if __name__=='__main__':
-    rospy.init_node('rl_node', anonymous=True)
-    
-    # With eager execution, operations are executed as they are 
-    # defined and Tensor objects hold concrete values, which 
-    # can be accessed as numpy.ndarray`s through the numpy() method.
-    tf.compat.v1.enable_eager_execution()
-
-    num_actions = 3 # commanded vertical velocity, roll and yaw
-    num_states = 3  
-
-    angle_max = 2.0 
-    angle_min = -2.0 # constraints for commanded roll and pitch
-    yaw_max = 5 #how much yaw should change every time
-    yaw_min = -5
-
-    max_vel_up = 1.5 # Real one is 2.5
-    max_vel_down = -1.5 # constraints for commanded vertical velocity
-
-    actor_model = get_actor()
-    print("Actor Model Summary")
-    print(actor_model.summary())
-
-    critic_model = get_critic()
-    print("Critic Model Summary")
-    print(critic_model.summary())
-
-    target_actor = get_actor()
-    target_critic = get_critic()
-
-    # Making the weights equal initially
-    target_actor.set_weights(actor_model.get_weights())
-    target_critic.set_weights(critic_model.get_weights())
-
-    # Load pretrained weights
-    # actor_model.load_weights('ddpg_actor.h5')
-    # critic_model.load_weights('ddpg_critic.h5')
-
-    # target_actor.load_weights('ddpg_target_actor.h5')
-    # target_critic.load_weights('ddpg_target_critic.h5')
-
-    # Learning rate for actor-critic models
-    critic_lr = 0.002
-    actor_lr = 0.001
-
-    # Define optimizer
-    critic_optimizer = tf.keras.optimizers.Adam(critic_lr)
-    actor_optimizer = tf.keras.optimizers.Adam(actor_lr)
-
-    # Discount factor for future rewards
-    gamma = 0.99
-    # Used to update target networks
-    tau = 0.005   
-
-    # To store reward history of each episode
-    ep_reward_list = []
-    # To store average reward history of last few episodes
-    avg_reward_list = [] 
-    episodes = []
-   
-    Environment()
-
-    # buffer = Buffer(100000, 1000)
-    buffer = Buffer(100000, 64)
-
-    std_dev = 0.1
-    # std_dev = 0.2
-
-    ou_noise = OUActionNoise(mean=np.zeros(num_actions), std_deviation=float(std_dev) * np.ones(num_actions))
-
-    r = rospy.Rate(20)
-    while not rospy.is_shutdown:
-        r.sleep()    
-
-    rospy.spin()        
-
